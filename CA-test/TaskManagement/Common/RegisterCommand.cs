150c691fd2f5026494a330a3dc2eca30ebe4be64
Hp <qnyazface@maul.ru>
2023-04-24 11:32:03 +0400

    Datatime added and some email funcionality fixed.

---
+++ CA-test/TaskManagement/Common/RegisterCommand.cs
@@ -0,0 +1,28 @@
+﻿using TaskManagement.Database;
+using TaskManagement.Database.Models;
+using TaskManagement.Database.Repositories;
+using TaskManagement.Utilities;
+
+
+namespace TaskManagement.Common
+{
+    public class RegisterCommand
+    {
+        public static void Handle()
+        {
+            UserRepository userRepository = new UserRepository();
+            UserValidator userValidator = new UserValidator();
+
+            string firstName = userValidator.GetAndValidateFirstName();
+            string lastName = userValidator. GetAndValidateLastName();
+            string password = userValidator.GetAndValidatePassword();
+            string email = userValidator.GetAndValidateEmail();
+
+
+            DateTime dateTime = DateTime.Now;
+            User human = new User(firstName, lastName, password, email);
+            Console.WriteLine($"Succes name: {human.Name} ,latsname:{human.LastName}, date:{human.RegistrationTime}");
+            userRepository.Insert(human);
+        }
+    }
+}
---
+++ CA-test/TaskManagement/Services/UserService.cs
@@ -0,0 +1,14 @@
+﻿wusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Database.Models;
+
+namespace TaskManagement.Services
+{
+    public class UserService
+    {
+        public static User CurrentUser { get; set; }
+    }
+}
